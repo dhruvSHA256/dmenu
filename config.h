41a83069b6779607c39ce3ac3efffa478f1aace1
Dhruv SHarma <dhruv.sah256@gmail.com>
2020-07-25 11:28:38 +0530

    changed fonts

--- config.h
+++ config.h
@@ -8,15 +8,16 @@ static unsigned int lineheight = 30;         /* -h option; minimum height of a m
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true","Twitter Color Emoji:style=Regular"};
+static const char *fonts[]          = { "JetBrains Mono NL:style=Regular:style=Regular:pixelsize=12:antialias=true:autohint=true","JoyPixels:pixelsize=18:style=Regular:antialias=true:autohint=true"};
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#333333" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	//[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeSelHighlight] = { "#6e42ab", "#333333" },
+	[SchemeNormHighlight] = { "#6e42ab", "#222222" },
 	[SchemeBorder] = {"#000000","#eeeeee"}, // not used value ,border color 
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -28,4 +29,4 @@ static unsigned int lines      = 0;
  */
 static const char worddelimiters[] = " ";
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 3;
