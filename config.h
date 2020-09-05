d2f79567ed1c2b6ba559a9e715684935c3d40183
DHruv <dhruv.sha256@gmail.com>
2020-09-05 12:20:34 +0530

    .

--- config.h
+++ config.h
@@ -2,25 +2,27 @@
 /* Default settings; can be overriden by command line. */
 
 static unsigned int lineheight =
-    30; /* -h option; minimum height of a menu line     */
+    18; /* -h option; minimum height of a menu line     */
 
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 
-static const char *fonts[] = {
-    "JetBrains Mono:style=Regular:size=10:antialias=true:autohint=true",
-    "JoyPixels:size=15:style=Regular:antialias=true:autohint=true",
-    };
+static const char *fonts[] = {"Symbols Nerd Font:style=2048-em:size=12",
+                              "Operator Mono Lig:style=medium:size=12",
+                              "JoyPixels:pixelsize=16:antialias=true:autohint=true"};
+/* static const char *fonts[] = {"Symbols Nerd Font:style=2048-em:size=12", */
+/*                               "Hurmit Nerd Font Mono:style=medium:size=12", */
+/*                               "JoyPixels:pixelsize=16:antialias=true:autohint=true"}; */
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm] = {"#aaaaaa", "#2E3440"},
-    [SchemeSel] = {"#F4F7F5", "#2f3440"},
+    [SchemeNorm] = {"#dFcFaF", "#2D2927"},
+    [SchemeSel] = {"#d9c9a0", "#131313"},
     [SchemeOut] = {"#000000", "#00ffff"},
-    [SchemeSelHighlight] = {"#abd76f", "#2E3440"},
-    [SchemeNormHighlight] = {"#bae67e", "#2E3440"},
-    [SchemeBorder] = {"#ffffff", "#ffe6b3"},  // not used value ,border color
+    [SchemeSelHighlight] = {"#EBE3CF", "#202223"},
+    [SchemeNormHighlight] = {"#EBE3CF", "#202223"},
+    [SchemeBorder] = {"#ffffff", "#ffe6b3"}, // not used value ,border color
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
@@ -31,4 +33,4 @@ static unsigned int lines = 0;
  */
 static const char worddelimiters[] = " ";
 /* Size of the window border */
-static const unsigned int border_width = 2;
+static const unsigned int border_width = 0;
